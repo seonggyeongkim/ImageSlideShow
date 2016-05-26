05895097331d079386747bf55a4b577dec82424d
Sunggyeong Kim <skim7902@gmail.com>
2016-05-26 20:13:11 +0900

    #2 Event Programming (MainActivity.java)

--- app/src/main/java/kr/hs/emirim/duckbill0316/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/duckbill0316/imageslideshow/MainActivity.java
@@ -2,12 +2,53 @@ package kr.hs.emirim.duckbill0316.imageslideshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{ // mainactivity를 핸들러 처리하는 감시자로 만듬
+                                                   //추상 메소드를 상속 받아서 사용함!
+
+    ViewFlipper flip; //1. 먼저 멤버변수 선언! (전역 변수로 선언)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flip=(ViewFlipper) findViewById(R.id.view_flip); //2. 멤버변수 값 지정!
+        flip.setFlipInterval(1000);//3. 멤버 변수 참조! (1초 후)
+
+        Button butStart=(Button)findViewById(R.id.but_start); //이벤트 발생 시키기
+        butStart.setOnClickListener(this); // 이벤트 핸들러(감시자) , 메인 액티비티를 사용하여 만듬!
+
+        Button butStop=(Button)findViewById(R.id.but_spot);
+        butStop.setOnClickListener(this);
+
+    }
+
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) { //view v 매개 변수
+
+        switch (v.getId()) //v.getId()를 사용하여 id 값 받아옴
+        {
+            case R.id.but_start :
+                flip.startFlipping();
+                break;
+            case R.id.but_spot :
+                flip.stopFlipping();
+                break;
+
+        }
+
     }
 }
